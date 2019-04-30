b632e4b79c362a19f83d5af2deae524e1d89b497
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-04-30 21:24:04 +0100

    Add autogenerated files to build SoftPosit

---
+++ build_tarballs.jl
@@ -0,0 +1,43 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "SoftPosit"
+version = v"0.4.1"
+
+# Collection of sources required to build SoftPosit
+sources = [
+    "https://gitlab.com/cerlane/SoftPosit/-/archive/0.4.1/SoftPosit-0.4.1.tar.gz" =>
+    "13f7360c5b91ad3704f66537a754ba3748a764e1291eaa33940866ca37c7dbf5",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd SoftPosit-0.4.1/build/Linux-x86_64-GCC/
+make julia
+cp softposit.* $prefix/
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:glibc),
+    FreeBSD(:x86_64)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "softposit", :libsoftposit)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
